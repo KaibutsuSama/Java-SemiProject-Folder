c394efe71c5196717f3a370db261d14879e07379
KaibutsuSama <XINLEROY.LIU001@STU.BMCC.CUNY.EDU>
2022-06-29 15:58:05 -0400

    feat: Update employee info

--- Takeout/src/main/java/com/kaibutsusama/reggie/config/WebMvcConfig.java
+++ Takeout/src/main/java/com/kaibutsusama/reggie/config/WebMvcConfig.java
@@ -1,9 +1,14 @@
 package com.kaibutsusama.reggie.config;
 
+import com.kaibutsusama.reggie.common.JacksonObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import java.util.List;
 
 /**
  * @author KaibutsuSama
@@ -14,10 +19,28 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupp
 @Configuration
 public class WebMvcConfig extends WebMvcConfigurationSupport {
 
+    /**
+     * 设置静态资源映射
+     * @param registry
+     */
     @Override
     protected void addResourceHandlers (ResourceHandlerRegistry registry) {
         log.info("开始static resource映射......");
         registry.addResourceHandler("/backend/**").addResourceLocations("classpath:/backend/");
         registry.addResourceHandler("/front/**").addResourceLocations("classpath:/front/");
+    }
+
+    /**
+     * 拓展mvc框架的消息转换器
+     * @param converters
+     */
+    @Override
+    protected void extendMessageConverters (List<HttpMessageConverter<?>> converters) {
+        //创建消息转换器对象
+        MappingJackson2HttpMessageConverter messageConverter = new MappingJackson2HttpMessageConverter();
+        //设置对象转换器,底层使用Jackson将Java对象转为Json
+        messageConverter.setObjectMapper(new JacksonObjectMapper());
+        //将上面的消息转换器追加到mvc框架的转换器集合中
+        converters.add(0,messageConverter);
     }
 }

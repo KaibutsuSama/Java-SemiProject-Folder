75a2472edd68cad702f40751061661395f547f30
KaibutsuSama <XINLEROY.LIU001@STU.BMCC.CUNY.EDU>
2022-06-28 23:00:22 -0400

    Debug: Cant add employee

--- Takeout/src/main/java/com/kaibutsusama/reggie/controller/EmployeeController.java
+++ Takeout/src/main/java/com/kaibutsusama/reggie/controller/EmployeeController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
 
 /**
  * @author KaibutsuSama
@@ -50,7 +51,7 @@ public class EmployeeController {
             return R.error("用户已被禁用");
         }
         // 5.登录成功,将EmployeeID存入Session并返回登录成功结果
-        request.getSession().setAttribute("employee",emp.getIdNumber());
+        request.getSession().setAttribute("employee",emp.getId());
         return R.success(emp);
     }
 
@@ -60,5 +61,27 @@ public class EmployeeController {
         //1. 清理session的登录员工的id
         request.getSession().removeAttribute("employee");
         return R.success("退出成功");
+    }
+
+    // Add Employee
+    @PostMapping
+    public R<String> save(HttpServletRequest request,@RequestBody Employee employee){
+        log.info("新增员工，员工信息：{}",employee.toString());
+
+        //设置初始密码123456，需要进行md5加密处理
+        employee.setPassword(DigestUtils.md5DigestAsHex("123456".getBytes()));
+
+        employee.setCreateTime(LocalDateTime.now());
+        employee.setUpdateTime(LocalDateTime.now());
+
+//        获得当前登录用户的id
+        Long empId = (Long) request.getSession().getAttribute("employee");
+
+        employee.setCreateUser(empId);
+        employee.setUpdateUser(empId);
+
+        employeeService.save(employee);
+
+        return R.success("新增员工成功");
     }
 }
